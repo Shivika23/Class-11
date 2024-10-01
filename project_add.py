8ad9603f6cc56b365d664a25a595431c92d712a0
Shivika23 <Shivika2310@gmail.com>
2024-10-01 16:51:22 +0530

    final commit

---
+++ project_add.py
@@ -0,0 +1,13 @@
+class Expression:
+    def __init__(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+    def calculate_sum(self):
+        result = self.num1 + self.num2 + self.num3
+        print(f"The sum of {self.num1}, {self.num2}, and {self.num3} is {result}.")
+
+obj1 = Expression(10, 20, 30)
+obj1.calculate_sum()
+
